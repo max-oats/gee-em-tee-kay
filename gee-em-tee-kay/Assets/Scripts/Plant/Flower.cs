f0d7f9667f32ad38d53007b1fa9ee2ff3aa2e22f
rjmcf <rjmcf@live.co.uk>
2019-08-08 23:47:32 +0100

    add intitial flower script

---
+++ gee-em-tee-kay/Assets/Scripts/Plant/Flower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    private float primaryColourHue;
+    private float highlightColourHue;
+    private float colourSaturation;
+    private float colourValue;
+
+}
