870ab9af2292f907ff7aa91982b8c28567418b10
rjmcf <rjmcf@live.co.uk>
2019-08-13 17:50:36 +0100

    Create Flower prefabs

--- gee-em-tee-kay/Assets/Scripts/Plant/Flower.cs
+++ gee-em-tee-kay/Assets/Scripts/Plant/Flower.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
+    [SerializeField] bool followsStem;
+
     private float primaryColourHue;
     private float highlightColourHue;
     private float colourSaturation;
     private float colourValue;
-
 }
