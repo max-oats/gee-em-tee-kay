7cfc0cee79233b62ff00cda3ba5e92ff4b401861
rjmcf <rjmcf@live.co.uk>
2019-08-13 12:46:48 +0100

    New Stem Section

--- gee-em-tee-kay/Assets/Scripts/Plant/StemSection.cs
+++ gee-em-tee-kay/Assets/Scripts/Plant/StemSection.cs
@@ -6,14 +6,10 @@ using UnityEditor;
 [RequireComponent(typeof(LineRenderer))]
 public class StemSection : MonoBehaviour
 {
-    public Vector3 end;
-    public Vector3 startTangent;
-    public Vector3 endTangent;
-
-    public Transform originPoint;
-    public Transform endPoint;
-
     [SerializeField] private LineRenderer lineRenderer;
+
+    [SerializeField] private GameObject startTangentMarker;
+    [SerializeField] private GameObject endTangentMarker;
     [SerializeField] private GameObject endPointMarker;
 
     [SerializeField] private bool drawGizmos = false;
@@ -21,35 +17,73 @@ public class StemSection : MonoBehaviour
     private int layerOrder = 0;
     private int SEGMENT_COUNT = 5;
 
+    public Transform GetStartPoint()
+    {
+        return transform.position;
+    }
+
+    public Transform GetStartTangent()
+    {
+        return startTangentMarker.transform.position;
+    }
+
+    public Transform GetEndTangent()
+    {
+        return endTangentMarker.transform.position;
+    }
+
+    public Transform GetEndPoint()
+    {
+        return endPointMarker.transform.position;
+    }
+
+    public void SetStartPoint(Vector3 inStartPoint)
+    {
+        transform.position = inStartPoint;
+    }
+
+    public void SetStartTangent(Vector3 inStartTangent)
+    {
+        startTangentMarker.transform.position = inStartTangent;
+    }
+
+    public void SetEndTangent(Vector3 inEndTangent)
+    {
+        endTangentMarker.transform.position = inEndTangent;
+    }
+
+    public void SetEndPoint(Vector3 inEndPoint)
+    {
+        endPointMarker.transform.position = inEndPoint;
+    }
+
     public void SetColour(Color inColor)
     {
         lineRenderer.material.color = inColor;
-    }
-
-    public void Realign()
-    {
-        gameObject.transform.rotation = originPoint.rotation;
     }
 
     void OnDrawGizmos()
     {
         if (drawGizmos)
         {
-            Vector3 p = transform.position;
             Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere(p, 0.1f);
-            Gizmos.DrawSphere(p + end, 0.1f);
+            Gizmos.DrawSphere(transform.position, 0.1f);
+            Gizmos.DrawSphere(endPointMarker.transform.position, 0.1f);
             Gizmos.color = Color.red;
-            Gizmos.DrawSphere(p + startTangent, 0.1f);
-            Gizmos.DrawSphere(p + endTangent, 0.1f);
+            Gizmos.DrawSphere(startTangentMarker.transform.position, 0.1f);
+            Gizmos.DrawSphere(endTangentMarker.transform.position, 0.1f);
         }
     }
 
     void Awake()
     {
+        startTangentMarker = new GameObject("startTangent");
+        endTangentMarker = new GameObject("endTangent");
         endPointMarker = new GameObject("endPoint");
-        endPointMarker.transform.parent = gameObject.transform;
-        endPoint = endPointMarker.transform;
+
+        startTangentMarker.transform.parent = transform;
+        endTangentMarker.transform.parent = transform;
+        endPointMarker.transform.parent = transform;
     }
 
     void Start()
@@ -64,16 +98,19 @@ public class StemSection : MonoBehaviour
     void Update()
     {
         DrawCurve();
-        endPoint.transform.position = gameObject.transform.position + end;
     }
 
     void DrawCurve()
     {
-        Vector3 p = gameObject.transform.position;
+        Vector3 p0 = transform.position;
+        Vector3 p1 = startTangentMarker.transform.position;
+        Vector3 p2 = endTangentMarker.transform.position;
+        Vector3 p3 = endPointMarker.transform.position;
+
         for (int i = 0; i <= SEGMENT_COUNT; i++)
         {
             float t = i / (float)SEGMENT_COUNT;
-            Vector3 pixel = BezierUtils.CalculateCubicBezierPoint(t, p, p + startTangent, p + endTangent, p + end);
+            Vector3 pixel = BezierUtils.CalculateCubicBezierPoint(t, p0, p1, p2, p3);
             lineRenderer.positionCount = i+1;
             lineRenderer.SetPosition(i, pixel);
         }
